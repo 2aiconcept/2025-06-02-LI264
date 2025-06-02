5abb09c4bad7746edc3ae9a5f00093bb037e1c9f
2aiconcept <contact@2aiconcept.com>
2025-06-02 15:25:40 +0200

    Etape 1.4

---
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -0,0 +1,55 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const DashboardPage = () => {
+  return (
+    <div className="container-fluid">
+      <h4>Laboratoire</h4>
+      <hr />
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink
+            to="jsxlab"
+            className={({ isActive }) =>
+              `nav-link   rounded ${isActive ? "active" : ""}`
+            }
+          >
+            Le Jsx
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="es6lab"
+            className={({ isActive }) =>
+              `nav-link   rounded ${isActive ? "active" : ""}`
+            }
+          >
+            Nouveautés ES6+
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="hookslab"
+            className={({ isActive }) =>
+              `nav-link   rounded ${isActive ? "active" : ""}`
+            }
+          >
+            Les hooks
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="propslab"
+            className={({ isActive }) =>
+              `nav-link   rounded ${isActive ? "active" : ""}`
+            }
+          >
+            Les Props
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Outlet />
+    </div>
+  );
+};
+export default DashboardPage;
